1ea0cc47fc58b533db79a0d6d3dcbaebab7137be
Дулепова Екатерина Имедовна <edulepova@gpkk.ru>
2026-02-09 11:38:55 +0700

    test2

---
+++ tests/.test1.js
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+
+test('check', async ({ page }) => {
+  await page.goto('https://https://www.google.com/');
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Google/);
+});
+
+test('get started link', async ({ page }) => {
+  await page.goto('https://https://www.google.com/');
